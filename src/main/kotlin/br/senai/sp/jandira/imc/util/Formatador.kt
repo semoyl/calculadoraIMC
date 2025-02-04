22e40095a2c7d826e3c4c878b1dff47d261453e3
GUSTAVO ROCHA GOMES SOUTO <24122785@dsn127m1242315.cfp127aluno.local>
2025-02-04 12:24:55 -0300

    Projeto iniciativo Finalizado

--- src/main/kotlin/br/senai/sp/jandira/imc/util/Formatador.kt
+++ src/main/kotlin/br/senai/sp/jandira/imc/util/Formatador.kt
@@ -4,9 +4,17 @@ import java.time.LocalDate
 
 fun formatarDataParaBrasil(data: LocalDate): String{
 
-    var dia = data.dayOfMonth
-    var mes = data.month.value
+    var dia = "${data.dayOfMonth}"
+    var mes = "${data.month.value}"
     var ano = data.year
+
+    if(dia.length == 1){
+        dia = "0$dia"
+    }
+
+    if(mes.length == 1){
+        mes = "0$mes"
+    }
 
     return "$dia/$mes/$ano"
 
